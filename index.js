6813dd6e0ff3278e353324ba0d81ff67efe26275
schazbot <charlotte.pearce1984@gmail.com>
2019-09-16 15:50:46 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution here
+const batteryFactory = [3, 4, 5, 6, 2, 1, 10]
+
+    const totalBatteries = batteryFactory.reduce((totalBatteries, batteryFactory) => totalBatteries + batteryFactory, 0)
